969254874e9871712efb146ad34b8efc11e9317a
Michal Fojtak <mfojtak@seznam.cz>
2017-04-13 14:53:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
-echo hello
+PERIOD=hourly
+
+TEMP=`getopt -o s:a::p:: --long script:,args::,period:: -- "$@"`
+eval set -- "$TEMP"
+
+while true; do
+  case "$1" in
+    -s | --script ) SCRIPT="$2"; shift 2 ;;
+    -a | --args ) ARGS="$2"; shift 2 ;;
+    -p | --period ) PERIOD="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+echo script=$SCRIPT
+echo args=$ARGS
+echo period=$PERIOD
+
+curl -o /etc/periodic/$PERIOD/script.sh $SCRIPT
+chmod a+x /etc/periodic/$PERIOD/script.sh
+crond -l 2 -f
