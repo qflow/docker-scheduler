309a722f731ad1dc8ff99e4a87ca31d17788f769
Michal Fojtak <mfojtak@seznam.cz>
2017-04-13 08:42:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-while getopts "script:args" opt; do
+PERIOD=hourly
+
+while getopts "script:args:period" opt; do
     case $opt in
         script)  SCRIPT="$OPTARG"
             ;;
         args)  ARGS="$OPTARG"
             ;;
+        period)  PERIOD="$OPTARG"
+            ;;
     esac
 done
 
-curl $SCRIPT
+curl -o /etc/periodic/$PERIOD/script.sh $SCRIPT
+chmod a+x /etc/periodic/$PERIOD/script.sh
+crond -l 2 -f
