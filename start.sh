aa2e9ac2cda604f47b19bfb7b3e16535429abd55
Michal Fojtak <mfojtak@seznam.cz>
2017-04-13 09:56:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,17 @@
 
 PERIOD=hourly
 
-while getopts "script:args:period" opt; do
-    case $opt in
-        script)  SCRIPT="$OPTARG"
-            ;;
-        args)  ARGS="$OPTARG"
-            ;;
-        period)  PERIOD="$OPTARG"
-            ;;
-    esac
+TEMP=`getopt -o s:a::p:: --long script:,args::,period:: -- "$@"`
+eval set -- "$TEMP"
+
+while true; do
+  case "$1" in
+    -s | --script ) SCRIPT="$2"; shift 2 ;;
+    -a | --args ) ARGS="$2"; shift 2 ;;
+    -p | --period ) PERIOD="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
 done
 
 curl -o /etc/periodic/$PERIOD/script.sh $SCRIPT
